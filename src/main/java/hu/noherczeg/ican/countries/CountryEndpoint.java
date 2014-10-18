e35dc840fe6e052f42fd799b7a38526994491b36
Norbert Csaba Herczeg <noherczeg@gmail.com>
2014-10-18 02:24:35 +0200

    added fault

--- src/main/java/hu/noherczeg/ican/countries/CountryEndpoint.java
+++ src/main/java/hu/noherczeg/ican/countries/CountryEndpoint.java
@@ -1,5 +1,6 @@
 package hu.noherczeg.ican.countries;
 
+import hu.noherczeg.ican.countries.io.Country;
 import hu.noherczeg.ican.countries.io.GetCountryRequest;
 import hu.noherczeg.ican.countries.io.GetCountryResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import org.springframework.ws.soap.server.endpoint.annotation.FaultCode;
+import org.springframework.ws.soap.server.endpoint.annotation.SoapFault;
 
 /**
  * ican
@@ -27,8 +30,16 @@ class CountryEndpoint {
     @ResponsePayload
     public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
         GetCountryResponse response = new GetCountryResponse();
+        Country country = countryRepository.findCountry(request.getName());
+        if (country == null) {
+            throw new MemberDetailsFault();
+        }
         response.setCountry(countryRepository.findCountry(request.getName()));
 
         return response;
+    }
+
+    @SoapFault(faultCode = FaultCode.SERVER, customFaultCode = "001", faultStringOrReason = "No Country found")
+    public class MemberDetailsFault extends RuntimeException{
     }
 }
